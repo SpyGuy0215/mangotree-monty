5ddfb061dbf457c712b263a63f55a2e895f29a0c
spyguy <shashankprasanna1@gmail.com>
2024-08-10 12:45:47 -0400

    Added logo, more menu fixes and items

--- app/page.js
+++ app/page.js
@@ -1,18 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className={'h-dvh w-dvh'}>
+        <div className={'h-dvh w-dvw'}>
             <div id={'menu'} className={'absolute z-10 bg-opacity-50 backdrop-blur-md w-full h-24 flex flex-row bg-gray-900'}>
-                <div id={'big-menu'} className={'invisible sm:visible w-1/3 ml-auto flex flex-row h-full text-xl justify-between px-8'}>
-                    <Link href={'/'} className={'h-fit w-fit my-auto'}>
+                <div id={'logo-container'} className={'relative aspect-square sm:ml-6'}>
+                    <Image src={'/logo.png'} alt={'Logo'} fill/>
+                </div>
+                <div id={'big-menu'}
+                     className={'invisible absolute text-center sm:relative sm:visible w-fit ml-auto flex flex-row h-full text-xl justify-between px-8'}>
+                    <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
                         <h2 className={'text-orange-400'}>Home</h2>
                     </Link>
-                    <Link href={'/'} className={'h-fit w-fit my-auto'}>
+                    <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
                         <h2 className={'text-slate-400 hover:text-orange-400'}>About Us</h2>
                     </Link>
+                    <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
+                        <h2 className={'text-slate-400 hover:text-orange-400'}>Our Menu</h2>
+                    </Link>
+                    <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
+                        <h2 className={'text-slate-400 hover:text-orange-400'}>Services</h2>
+                    </Link>
+                    <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
+                        <h2 className={'text-slate-400 hover:text-orange-400'}>Gallery</h2>
+                    </Link>
                 </div>
-                <div id={'small-menu'} className={'visible sm:invisible sm:w-0 sm:h-0 w-1/4'}>
+                <div id={'small-menu'} className={'visible sm:invisible sm:w-0 sm:h-0 w-fit border-blue-400'}>
 
                 </div>
             </div>
