3885b08ced2f58e2ed2b19e3a5588c90fddfd852
spyguy <shashankprasanna1@gmail.com>
2024-09-21 18:49:54 -0400

    Ready for restaurant opening - added order page, footer, flyout menu for mobile, contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,15 +1,14 @@
 'use client';
-
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import {Divide as Hamburger} from "hamburger-react";
 import {useState} from "react";
 
-export default function Home() {
+export default function Contact() {
 
     const [isFlyoutOpen, setFlyoutOpen] = useState(false);
 
-    return (
+    return(
         <div className={'h-dvh dark:[color-scheme:dark]'}>
             <div id={'menu-container'} className={'absolute z-20 w-full h-full flex flex-col'}>
                 <div id={'top-menu'} className={'w-full h-24 absolute z-20 flex flex-row backdrop-blur-md bg-opacity-50 bg-gray-900'}>
@@ -19,7 +18,7 @@ export default function Home() {
                     <div id={'big-menu'}
                          className={'invisible w-0 h-0 absolute text-center sm:relative sm:visible sm:w-fit ml-auto flex flex-row sm:h-full text-xl justify-between px-8'}>
                         <Link href={'/'} className={'h-fit w-fit my-auto px-3 absolute sm:relative'}>
-                            <h2 className={'text-orange-400'}>Home</h2>
+                            <h2 className={'text-slate-400 hover:text-orange-400'}>Home</h2>
                         </Link>
                         <Link href={'https://www.clover.com/online-ordering/mango-tree-indian-cuisin-belle-mead'}
                               className={'h-fit w-fit my-auto px-3 absolute sm:relative'}>
@@ -27,7 +26,7 @@ export default function Home() {
                         </Link>
                         <Link href={'/contact'}
                               className={'h-fit w-fit my-auto px-3 absolute sm:relative'}>
-                            <h2 className={'text-slate-400 hover:text-orange-400'}>Contact Us</h2>
+                            <h2 className={'text-orange-400'}>Contact Us</h2>
                         </Link>
                         {/*<Link href={'/'} className={'h-fit w-fit my-auto px-3 absolute sm:relative'}>*/}
                         {/*    <h2 className={'text-slate-400 hover:text-orange-400'}>About Us</h2>*/}
@@ -44,16 +43,16 @@ export default function Home() {
                             <h2 className={'text-slate-50 hover:text-white py-4'}>Order Now</h2>
                         </Link>
                     </div>
-                    <div className={'scale-125 w-fit h-fit z-40 md:scale-0 md:w-0 md:-z-20 self-center absolute right-7'}>
+                    <div className={'scale-125 w-fit h-fit z-40 md:scale-0 md:w-0 md:-z-10 self-center absolute right-7'}>
                         <Hamburger toggled={isFlyoutOpen} toggle={setFlyoutOpen} size={48} color={'#FFFFFF'}/>
                     </div>
                 </div>
                 <div id={'flyout'}
-                     className={'h-full z-20 mt-24 backdrop-blur-md bg-opacity-50 bg-gray-900 relative'}
+                     className={'h-full z-20 md:-z-20 mt-24 backdrop-blur-md bg-opacity-50 bg-gray-900 relative'}
                      hidden={!isFlyoutOpen}>
                     <div id={'flyout-link-manager'} className={'h-80 text-4xl mx-auto flex flex-col items-center'}>
                         <Link href={'/'} className={'h-fit w-fit my-auto px-3'}>
-                            <h2 className={'text-orange-400'}>Home</h2>
+                            <h2 className={'text-slate-400 hover:text-orange-400'}>Home</h2>
                         </Link>
                         <Link href={'https://www.clover.com/online-ordering/mango-tree-indian-cuisin-belle-mead'}
                               className={'h-fit w-fit my-auto px-3 '}>
@@ -61,7 +60,7 @@ export default function Home() {
                         </Link>
                         <Link href={'/contact'}
                               className={'h-fit w-fit my-auto px-3'}>
-                            <h2 className={'text-slate-400 hover:text-orange-400'}>Contact Us</h2>
+                            <h2 className={'text-orange-400'}>Contact Us</h2>
                         </Link>
                         <Link href={'https://www.clover.com/online-ordering/mango-tree-indian-cuisin-belle-mead'}
                               className={'bh-fit w-fit my-auto px-3  bg-orange-400 ml-1 hover:scale-110 transition ease-in-out'}>
@@ -70,9 +69,28 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div id={'section-1'} className={'h-fit bg-black'}>
-                <img className={'object-cover h-svh mx-auto'} src={'/opening-9-22-large-resized.png'}
-                     alt={'The Mango Tree - Opening 9/22'}/>
+            <div id={'section-1'} className={'w-full h-fit py-32 text-center'}>
+                <h2 className={'text-5xl'}>Contact Us</h2>
+            </div>
+            <div id={'section-2'} className={'flex flex-col md:flex-row mx-20 mb-20 justify-between text-center'}>
+                <div id={'phone-box'} className={'flex flex-col bg-zinc-900 pb-20 pl-10 pr-10 rounded-3xl border-2 border-zinc-200 mt-10'}>
+                    <Image src={'/phone-icon.png'} alt={'Phone Icon'} width={32} height={32} className={'invert mx-auto my-10'}/>
+                    <h2 className={'text-3xl py-2'}>Phone Number</h2>
+                    <h2 className={'text-md md:text-4xl mt-10'}>(908)-829-3169</h2>
+                </div>
+                <div id={'address-box'} className={'flex flex-col bg-zinc-900 pb-20 pl-10 pr-10 rounded-3xl border-2 border-zinc-200 mt-10'}>
+                    <Image src={'/location-pin-icon.png'} alt={'Location Pin Icon'} width={32} height={32} className={'invert mx-auto my-10'}/>
+                    <h2 className={'text-3xl py-2'}>Location</h2>
+                    <h2 className={'text-md md:text-4xl mt-10'}>2311 Route 206, Suite 102, Belle Mead NJ</h2>
+                </div>
+                <div id={'hours-box'} className={'flex flex-col bg-zinc-900 pb-20 pl-10 pr-10 rounded-3xl border-2 border-zinc-200 mt-10'}>
+                    <Image src={'/clock-icon.png'} alt={'Clock Icon'} width={32} height={32} className={'invert mx-auto my-10'}/>
+                    <h2 className={'text-3xl py-2'}>Hours of Operation</h2>
+                    <h3 className={'text-md md:text-xl pt-2 pb-1'}>Wednesday - Monday</h3>
+                    <h3 className={'text-sm md:text-md py-1'}>11:30AM - 2:30PM</h3>
+                    <h3 className={'text-sm md:text-md py-1'}>5:30PM - 9:30PM</h3>
+                    <h3 className={'text-md md:text-xl py-2'}>Tuesday: Closed</h3>
+                </div>
             </div>
             <div id={'footer'} className={'flex flex-col mb-20 md:pb-0 pt-8 md:pt-0 md:flex-row h-60 bg-black items-center'}>
                 <div id={'box-1 contact-footer'} className={'flex flex-col text-white w-full md:w-5/12 text-center'}>
@@ -82,15 +100,18 @@ export default function Home() {
                         <h2 className={'text-md md:text-xl'}>(908)-829-3169</h2>
                     </div>
                     <div id={'contact-footer-phone'} className={'flex flex-row w-fit self-center justify-between py-2'}>
-                        <Image src={'/location-pin-icon.png'} alt={'Location Icon'} width={24} height={0} className={'invert aspect-square mr-2'}/>
+                        <Image src={'/location-pin-icon.png'} alt={'Location Icon'} width={24} height={0}
+                               className={'invert aspect-square mr-2'}/>
                         <h2 className={'text-md md:text-xl'}>2311 Route 206, Suite 102, Belle Mead NJ</h2>
                     </div>
                     <div id={'contact-footer-socials'} className={'flex flex-row w-fit self-center justify-between py-2'}>
                         <Link href={'https://www.facebook.com/share/f3s93fH55GRMavx1/?mibextid=LQQJ4d'}>
-                            <Image src={'/facebook-icon.png'} alt={'Facebook Icon'} width={40} height={40} className={'aspect-square mr-2'}/>
+                            <Image src={'/facebook-icon.png'} alt={'Facebook Icon'} width={40} height={40}
+                                   className={'aspect-square mr-2'}/>
                         </Link>
                         <Link href={'https://www.instagram.com/themangotreemonty?igsh=dHpiMHF3MXBwYnRz'}>
-                            <Image src={'/instagram-icon.png'} alt={'Instagram Icon'} width={42} height={42} className={'aspect-square mr-2'}/>
+                            <Image src={'/instagram-icon.png'} alt={'Instagram Icon'} width={42} height={42}
+                                   className={'aspect-square mr-2'}/>
                         </Link>
                     </div>
                 </div>
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,22 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import {Analytics} from "@vercel/analytics/next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Mango Tree",
-  description: "",
+  title: "The Mango Tree Indian Cuisine",
+  description: "The Mango Tree, coming soon to Montgomery. A brand new Indian restaurant serving quality Indian food.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
