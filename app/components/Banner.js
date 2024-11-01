8f889373bf9ed53556947630969da5185f02f236
spyguy <shashankprasanna1@gmail.com>
2024-11-01 19:24:33 -0400

    Added Diwali Buffet page, changed Banner

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,18 +1,22 @@
+import Link from "next/link";
+
 export default function Banner() {
     /*
     * This component is a banner that appears at the top of the page (above header).
     * It is used to display important information to the user.
      */
-    let isBannerVisible = false; // Turn false when not needed
+    let isBannerVisible = true; // Turn false when not needed
 
-    if(isBannerVisible){
-        return (
-            <div id={'banner'} className={'w-full h-12 bg-black text-white flex flex-row justify-center items-center'}>
-                <h2 className={'text-center text-md md:text-2xl text-orange-300'}>All tables booked from 12:15pm - 2pm. Expect a wait time of at least 1 hour.</h2>
-            </div>
-        )
-    }
-    else{
+    if (isBannerVisible) {
+        return (<div id={'banner'} className={'w-full h-12 bg-black text-white flex flex-row justify-center items-center'}>
+
+                <Link href={'/diwalibuffet'} className={''}>
+                    <h2 className={'text-center text-md md:text-2xl text-orange-300 underline underline-offset-2 hover:text-orange-100'}>
+                        Happy Diwali! Special Diwali Buffet on Saturday, Nov. 2nd
+                    </h2>
+                </Link>
+            </div>)
+    } else {
         return null;
     }
 }
