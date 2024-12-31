f0cae29fe3922b99552c0ae51045279111d856c9
spyguy <shashankprasanna1@gmail.com>
2024-12-31 12:10:54 -0500

    New Year's Banner.

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,20 +1,15 @@
-import Link from "next/link";
-
 export default function Banner() {
     /*
     * This component is a banner that appears at the top of the page (above header).
     * It is used to display important information to the user.
      */
-    let isBannerVisible = false; // Turn false when not needed
+    let isBannerVisible = true; // Turn false when not needed
 
     if (isBannerVisible) {
         return (<div id={'banner'} className={'w-full h-12 bg-black text-white flex flex-row justify-center items-center'}>
-
-                <Link href={'/diwalibuffet'} className={''}>
-                    <h2 className={'text-center text-md md:text-2xl text-orange-300 underline underline-offset-2 hover:text-orange-100'}>
-                        Happy Diwali! Special Diwali Buffet on Saturday, Nov. 2nd
-                    </h2>
-                </Link>
+                <h2 className={'text-center text-md md:text-3xl text-orange-300 underline underline-offset-2'}>
+                    Happy New Year! Today, we are open until 11:30PM. BYOB.
+                </h2>
             </div>)
     } else {
         return null;
